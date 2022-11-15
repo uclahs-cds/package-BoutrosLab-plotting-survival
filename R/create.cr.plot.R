1fb4be6fd43c182c46a2713eebfbbee3fd9e1b50
Tian <maotian@ip-0A12520C.rhxrlfvjyzbupc03cc22jkch3c.xx.internal.cloudapp.net>
2022-11-15 15:41:19 -0800

    add 2nd version

--- R/create.cr.plot.R
+++ R/create.cr.plot.R
@@ -1,3 +1,17 @@
+### create.cr.plot.R ######################################################################
+# Description:
+#       Produces a competing risk (CR) plot for any number of risk groups, as well as a
+#       corresponding risk table.
+# Input variables:
+#   data: a data.frame contains `time`, `status`, and `group` information
+#   formula: time ~ status
+#            time: numeric values, representing months or years;
+#            status: numeric values, such as 0, 1, 2, 3 ..., representing different groups
+#   patient.groups: the colname of group information
+
+# Output variables: If no filename is specified returns a trellis object.  If a filename is
+#       specified, prints the trellis object to a plot.pdf file
+
 create.cr.plot <- function(
     data,
     formula,
@@ -15,6 +29,7 @@ create.cr.plot <- function(
     style = 'BoutrosLab',
     resolution = 1000
     ) {
+
     # create CIF curve estimates
     fit.time <- data[,toString(formula[[2]])];
     fit.status <- data[,toString(formula[[3]])];
@@ -39,16 +54,16 @@ create.cr.plot <- function(
     df <- data.frame();
     suppressMessages(
         for (i in 1:(length(fit) - 1)) {
-            simplify.fit <- data.frame(simplify2array(fit[[i]]));
-            simplify.fit$type <- names(fit)[i];
-            if (dim(df)[1] == 0 ){
-                df <-  simplify.fit;
-                rm(simplify.fit);
-            } else {
-                df <-  rbind(df, simplify.fit);
-                rm(simplify.fit);
-                }
-            }
+        simplify.fit <- data.frame(simplify2array(fit[[i]]));
+        simplify.fit$type <- names(fit)[i];
+        if (dim(df)[1] == 0 ){
+            df <-  simplify.fit;
+            rm(simplify.fit);
+        } else {
+            df <-  rbind(df, simplify.fit);
+            rm(simplify.fit);
+        }
+        }
     )
     colnames(df) <- c('Time', 'CIF', 'Variance', 'Group');
 
@@ -70,7 +85,7 @@ create.cr.plot <- function(
         groups = Group,
         type = c('l','p'),
         col = as.vector(rbind(line.colours, line.colours)),
-        pch = "|",
+        pch = '|',
         lty = as.numeric(gsub('.*[ ](.*)', '\\1', unique(df$Group))),
         lwd = 5,
         right.padding = 2,
@@ -85,16 +100,16 @@ create.cr.plot <- function(
             lab = unique(df$Group),
             cex = 1,
             col = 'black'
-        ),
+            ),
         lines = list(
             lty = as.numeric(gsub('.*[ ](.*)', '\\1', unique(df$Group))),
             col = as.vector(cbind(line.colours, line.colours)),
             lwd = 5
-        ),
+            ),
         x = 0.04,
         y = 0.95,
         padding.text = 2
-        )
+        );
     )
     return(cr.plot);
     }
